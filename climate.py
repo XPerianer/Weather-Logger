bb1e35e99247188ddd94daf11574344cefcc3a97
XPerianer <nikmeier@hotmail.de>
2017-09-25 20:58:58 +0000

    Made allowed difference smaller

--- climate.py
+++ climate.py
@@ -11,7 +11,7 @@ h1 = j['humidity']
 h2 = jj['humidity']
 t1 = j['temperature']
 t2 = jj['temperature']
-if(abs(h1 - h2) > 5 or abs(t1 - t2) > 5 or t1 < -200 or t2 < -200 or h1 < -200 or h2 < -200):
+if(abs(h1 - h2) > 2 or abs(t1 - t2) > 2 or t1 < -200 or t2 < -200 or h1 < -200 or h2 < -200):
  print("Problem")
 else:
  str = json.dumps(jj)
