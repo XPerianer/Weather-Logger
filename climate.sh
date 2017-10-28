2323234a2273e89edbd7b7133d1b877985c31838
root <root@Omega-FF07.lan>
2017-10-29 00:30:14 +0200

    ÃUpdtated sh

--- climate.sh
+++ climate.sh
@@ -2,9 +2,9 @@
 
 t1=$(date +%s)
 t1=$(($t1 + 30)) 
-/usr/bin/python /root/climate.py
+/usr/bin/python /root/Weather-Logger/climate.py
 t2=$(date +%s)
 sleep_seconds=$(($t1-$t2))
 echo $sleep_seconds
 sleep $sleep_seconds
-/usr/bin/python /root/climate.py
+/usr/bin/python /root/Weather-Logger/climate.py
